dabcd2a9202bb957d1b46b2b96b4a78935b44f58
fatelei <fatelei@gmail.com>
2020-03-29 17:01:46 +0800

    feat: set log to local timezone

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,11 +6,13 @@ import (
 	"github.com/fatelei/juzimiaohui-webhook/configs"
 	"github.com/fatelei/juzimiaohui-webhook/pkg/connection"
 	"github.com/fatelei/juzimiaohui-webhook/web"
+	"log"
 )
 
 func main() {
 	var port int
 	var configFile string
+	log.SetFlags(log.Ldate)
 	flag.IntVar(&port, "port", 8000, "http server port")
 	flag.StringVar(&configFile,"config", "/etc/webhook.toml", "webhook config path")
 	flag.Parse()

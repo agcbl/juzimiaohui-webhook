049bcd2df237ed30229f70a51bc50d50aa2a9cd1
fatelei <fatelei@gmail.com>
2020-04-21 07:42:10 +0800

    fix: fixme

--- pkg/dao/impl/wechat_message.go
+++ pkg/dao/impl/wechat_message.go
@@ -56,7 +56,7 @@ func (p *WechatMessageDAOImpl) GetRecentMessages(wxid string, roomId string, cre
 	if direction == "before" {
 		stmtQuery, err = connection.DB.Prepare(
 			`SELECT id, wxid, wechat_name, room_name, content, msg_type, created_at, room_id, message_id FROM wechat_message_monitor
-	WHERE wxid = ? AND room_id = ? AND created_at <= ? order by desc limit 10`)
+	WHERE wxid = ? AND room_id = ? AND created_at <= ? order by id desc limit 10`)
 	} else {
 		stmtQuery, err = connection.DB.Prepare(
 			`SELECT id, wxid, wechat_name, room_name, content, msg_type, created_at, room_id, message_id FROM wechat_message_monitor
